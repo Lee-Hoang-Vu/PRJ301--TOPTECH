ae7aafcce321b0a059419fccec703b1d3809e827
tuyennqhe161645 <tuyennqhe161645@fpt.edu.vn>
2023-03-12 23:31:51 +0700

    fix1.2

--- Group3_PRJ301/src/java/controller/VerifyEmail.java
+++ Group3_PRJ301/src/java/controller/VerifyEmail.java
@@ -24,7 +24,7 @@ public class VerifyEmail extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
+       this.doPost(req, resp);
     }
     
     void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
